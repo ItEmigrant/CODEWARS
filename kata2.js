b18aa8c277a15db0c0a35f71799d91e224c68ea8
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-17 15:46:35 +0200

    katafriends2.1.js

---
+++ kata2.js
@@ -0,0 +1,14 @@
+function friend(friends){
+    let input = ["Ryan", "Kieran", "Mark", "Jason", "Yous", "Ryan"];
+    let friends = [];
+    for (let i = 0; i < input.length; i++) {
+        if (i = 4) {
+            console.log(friends);
+        }
+        else {
+
+        }
+    }
+
+}
+friend(friends)();
