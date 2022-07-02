235f97a14d1c64ec3c953ef97a29437119e92903
Bogdan Sol <bohdan.jur@gmail.com>
2022-07-02 22:15:43 +0200

    kata21.js

---
+++ kata21.js
@@ -0,0 +1,10 @@
+function opposite(number) {
+    //your code here
+    if (number<0) {
+      
+      return number = number - (number*2);}
+  
+      else 
+  {
+    return number = 0-number;}
+  }
