baa71b99f3c2a5bd11811eb17c714bd36a64a936
Bogdan Sol <bohdan.jur@gmail.com>
2022-07-08 22:41:57 +0200

    kata27-28

---
+++ kata27sheep.js
@@ -0,0 +1,10 @@
+function countSheeps(arrayOfSheep) {
+    
+    var num = 0;
+    
+    for(var i = 0; i < arrayOfSheep.length; i++)
+      if(arrayOfSheep[i] == true)
+        num++;
+        
+    return num;
+  }
