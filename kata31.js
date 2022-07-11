913aa5289579f6790867225630b7d76caa042478
Bogdan Sol <bohdan.jur@gmail.com>
2022-07-11 11:19:22 +0200

    kata31

---
+++ kata31.js
@@ -0,0 +1,20 @@
+function sum(numbers) {
+    let sum = 0;  
+    
+    for (let i = 0; i < numbers.length; i++)  {
+      
+      if (numbers == '') {
+      return 0;
+    }
+      else {
+         sum += numbers[i]; }
+    }
+      return sum;
+      
+  
+   } 
+     
+
+   
+
+  
