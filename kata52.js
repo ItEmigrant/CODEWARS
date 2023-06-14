e6b69608d77050ea42025e14c6076734fee50f62
Bohdan Solomchak <bohdan.jur@gmail.com>
2023-06-14 14:41:32 +0200

    kata 52

---
+++ kata52.js
@@ -0,0 +1,10 @@
+//kata52
+function problem(x) {
+    if (typeof x !== "number") {
+        return 'Error'
+    } else {
+        let y = x * 50 + 6;
+       return y
+    }
+    //your code here
+}
