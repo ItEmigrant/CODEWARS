94a93c692600d3ef1a132d3e77ba67bf2a38e9fe
Bohdan Solomchak <bohdan.jur@gmail.com>
2024-01-02 19:35:49 +0100

    kata53and54Refactoring

--- kata53.js
+++ kata53.js
@@ -1,13 +1,17 @@
 //kata53
 //If/else syntax debug
-function checkAlive (health) {
-    if (health <= 0) {    
+/*function checkAlive (health) {
+    if (health <= 0) {
       return false
     } else {
       return true
     }
   }
 
-console.log(checkAlive (5))
+console.log(checkAlive (5))*/
 
-  
+function checkAlive (health) {
+    return health > 0;
+}
+
+console.log(checkAlive (5))
--- kata54.js
+++ kata54.js
@@ -1,9 +1,9 @@
-let quote = function (fighter) {
+/*let quote = function (fighter) {
     const a = ['conor mcgregor', 'Conor McGregor']
-    return a.includes(fighter.toLowerCase()) ? "I'd like to take this chance to apologize.. To absolutely NOBODY!" : "I am not impressed by your performance."
+    return a.includes(fighter.toLowerCase()) ? "I'd like to take this chance to apologize. To absolutely NOBODY!"  "I am not impressed by your performance."
 
 };
-console.log(quote('george saint pierre'))
+console.log(quote('george saint pierre'))*/
 
 //OK VERSION
 function quote(fighter) {
